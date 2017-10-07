3a2a9f4f2dccaf79f28af570a7cefc3f98ffd02b
Jon Eyrick <social@nextlocal.net>
2017-10-07 00:25:47 -0700

    Added Kline/candlestick data
    Visit binance.com

--- node-binance-api.js
+++ node-binance-api.js
@@ -154,6 +154,9 @@ module.exports = function() {
 		trades: function(symbol,callback) {
 			signedRequest(base+"v3/myTrades", {symbol:symbol}, callback);
 		},
+		candlesticks: function(symbol, interval = "5m", callback) { //1m,3m,5m,15m,30m,1h,2h,4h,6h,8h,12h,1d,3d,1w,1M
+			publicRequest(base+"v1/klines", {symbol:symbol, interval:interval}, callback);
+		},
 		publicRequest: function(url, data, callback, method = "GET") {
 			publicRequest(url, data, callback, method)
 		},
