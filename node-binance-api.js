cc64911e5ced228d71c0ebd7e9ea900e293f2f6a
Simon Gemmell <gemmell@athoughtadrift.com>
2018-05-04 15:25:19 +1000

    Added rawTrades to the ws api as a copy-paste-and-change of trades.

--- node-binance-api.js
+++ node-binance-api.js
@@ -1675,7 +1675,7 @@ module.exports = function() {
             },
 
             /**
-            * Websocket trades
+            * Websocket aggregated trades
             * @param {array/string} symbols - an array or string of symbols to query
             * @param {function} callback - callback function
             * @return {string} the websocket endpoint
@@ -1698,9 +1698,34 @@ module.exports = function() {
                 }
                 return subscription.endpoint;
             },
+            
+            /**
+            * Websocket raw trades
+            * @param {array/string} symbols - an array or string of symbols to query
+            * @param {function} callback - callback function
+            * @return {string} the websocket endpoint
+            */
+            rawTrades: function rawTrades(symbols, callback) {
+                let reconnect = function() {
+                    if ( options.reconnect ) trades(symbols, callback);
+                };
+
+                let subscription;
+                if ( Array.isArray(symbols) ) {
+                    if ( !isArrayUnique(symbols) ) throw Error('trades: "symbols" cannot contain duplicate elements.');
+                    let streams = symbols.map(function(symbol) {
+                        return symbol.toLowerCase()+'@trade';
+                    });
+                    subscription = subscribeCombined(streams, callback, reconnect);
+                } else {
+                    let symbol = symbols;
+                    subscription = subscribe(symbol.toLowerCase()+'@trade', callback, reconnect);
+                }
+                return subscription.endpoint;
+            },
 
             /**
-            * Websocket trades
+            * Websocket klines
             * @param {array/string} symbols - an array or string of symbols to query
             * @param {string} interval - the time interval
             * @param {function} callback - callback function
