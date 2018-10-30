900199a4320150499b926f43dcbbdb90d5084bc4
Jon Eyrick <social@nextlocal.net>
2018-10-30 12:19:42 +0100

    Example: Connect to all WebSocket trade endpoints

---
+++ examples/experiments.js
@@ -0,0 +1,21 @@
+// Connect to all websocket endpoints without using the library
+(async () => {
+  const axios = require('axios');
+  const WebSocket = require('ws');
+  let streams = [];
+  let markets = [];
+  let prevDay = await axios.get('https://api.binance.com/api/v1/ticker/24hr');
+  for ( let obj of prevDay.data ) {
+    markets.push(obj.symbol);
+    streams.push(obj.symbol.toLowerCase()+'@aggTrade');
+  }
+  console.log(markets);
+  
+  const ws = new WebSocket('wss://stream.binance.com:9443/stream?streams='+streams.join('/'));
+  ws.on('message', function(payload) {
+    let json = JSON.parse(payload), stream = json.stream, data = json.data;
+    if ( data.e == 'aggTrade' ) {
+      console.log(data.s+' price: '+data.p);
+    } else console.log(stream, data);
+  });
+})();
