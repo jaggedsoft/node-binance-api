88aca5f6d0f842f2874a6fe1ef9f5cbc36577965
Eluvade <bunny.eluvade@gmail.com>
2019-09-14 13:20:52 +0200

    Removed unnecessary semicolon.

--- node-binance-api.js
+++ node-binance-api.js
@@ -1499,7 +1499,7 @@ let api = function Binance() {
                     return resolve(result);
                   });
                 });
-            };
+            }
             request(addProxy(opt), function (error, response, body) {
                 if (error) return callback(error);
                 if (response.statusCode !== 200) return callback(response);
